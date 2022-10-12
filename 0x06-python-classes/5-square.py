ce4b2e3499b27fd24a4c671b6d81fa0495f83b1a
jamiukabir22 <kabirm474@gmail.com>
2022-10-12 00:13:52 -0700

    square 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,60 +4,57 @@
 
 class Square:
     """Represents a square
-
     Attributes:
-    __size (int): size of the sides to a square
+        __size (int): size of the sides of a square
     """
     def __init__(self, size=0):
         """Initializes the Square
         Args:
-        size (int): size of sides to a square
+            size (int): size of sides of square
         Returns:
-        None
+            None
         """
         self.size = size
 
-        def area(self):
-            """Calcukates the square's area
-
-            Returns:
+    def area(self):
+        """Calculates the square's area
+        Returns:
             Area
-            """
-            return (self.__size) ** 2
+        """
+        return (self.__size) ** 2
 
-        @property
-        def size(self):
-            """getter of size
-            Returns:
-            The size of square
-            """
-            return self.__size
+    @property
+    def size(self):
+        """getter of size
+        Returns:
+           The size of square
+        """
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """setter of size
-            Args:
-            value (int): The size of the sides to a square
-            Returns:
+    @size.setter
+    def size(self, value):
+        """Setter of size
+        Args:
+            value (int): The size of the sides of the square
+        Returns:
             None
-            """
-            if type(value) is not int:
-                raise typeError("size must be an integer")
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
             else:
-                if value < 0:
-                    raise ValueError("size must be >= 0")
-                else:
-                    self.__size = value
+                self.__size = value
 
-                    def my_print(self):
-                        """Prints the square
-
-                        Returns:
-                        None
-                        """
-                        if self.__size == 0:
-                            print()
-                            return
-                        else:
-                            form i in range(self.__size):
-                                print("".join(["#" for j in range(self.__size)]))
+    def my_print(self):
+        """Prints the square
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+            return
+        else:
+            for i in range(self.__size):
+                print("".join(["#" for j in range(self.__size)]))
