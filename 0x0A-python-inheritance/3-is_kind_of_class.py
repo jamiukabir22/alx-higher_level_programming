0d5bf92784a703a502f7764305a861e8e2dce0d9
jamiukabir22 <kabirm474@gmail.com>
2022-10-25 03:06:51 -0700

    Returns Turn if object is an instance else False

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A function that returns True if object is exactly
+an instance of specified class else False
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Returns True of False"""
+    return (isinstance(obj, a_xlass))
