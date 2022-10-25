2b18c432b0e45b9eabe61501d31fac735020a37d
jamiukabir22 <kabirm474@gmail.com>
2022-10-25 01:08:48 -0700

    main file

---
+++ 0x0A-python-inheritance/1-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+MyList = __import__('1-list').MyList
+
+my_list = MyList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+my_list.print_sorted()
+print(my_list)
---
+++ 0x0A-python-inheritance/5-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('5-base_geametry').BaseGeometry
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
