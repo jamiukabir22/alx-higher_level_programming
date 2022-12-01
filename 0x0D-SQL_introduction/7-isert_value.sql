cd329663aedcfb649da19c8a3d77772edfa7fe27
jamiukabir22 <kabirm474@gmail.com>
2022-12-01 14:12:54 -0800

    done

--- 0x0D-SQL_introduction/7-isert_value.sql
+++
@@ -1,5 +0,0 @@
--- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
--- New row: id = 89; name = Best School
--- Database name will be passed as an argument of mysql command
-
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
