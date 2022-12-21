9b9897bc039dd47e3591b5bf85aa1074f4175ff3
jamiukabir22 <kabirm474@gmail.com>
2022-12-21 13:14:17 -0800

    prints x times “C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const args = process.argv;
 if (isNaN(Number(args[2]))) {
 	console.log('Missing number of occurances');
 } else {
-	for (let h = 0; h < NUmber(args[2]); h++) {
+	for (let h = 0; h < Number(args[2]); h++) {
 		console.log('C is fun');
 	}
 }
