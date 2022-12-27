379f0779247b7535149e343a379a8bfd1fe1620a
jamiukabir22 <kabirm474@gmail.com>
2022-12-27 14:19:38 -0800

    completed

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,7 +7,7 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
-form model_state import Base, State
+from model_state import Base, State
 
 if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3])))
